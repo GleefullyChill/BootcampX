536979d6af76e438e2df5e381039161e2da4e6cc
GledfullyChill <stephan.lofstrom@gmail.com>
2021-08-18 20:18:13 -0600

    sanitatization coding to database queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,13 +8,14 @@ const pool = new Pool({
 });
 const cohort = process.argv[2];
 const limit = process.argv[3];
+const values = [`%${cohort}%`, limit]
 pool.query(`
 SELECT students.id, students.name student, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${cohort}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   console.log(res.rows);
 })
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,16 +6,16 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-const cohort = process.argv[2];
+const cohort = `${process.argv[2]}`;
 pool.query(`
   SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
   FROM assistance_requests
   JOIN teachers ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = '${cohort}'
+  WHERE cohorts.name = $1
   ORDER BY teacher;
-`)
+`, [cohort])
 .then(res => {
   console.log(res.rows);
 })
