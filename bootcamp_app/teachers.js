6d97e9e41aa7b99b92c71fec01b23a777f66758d
GledfullyChill <stephan.lofstrom@gmail.com>
2021-08-18 20:01:50 -0600

    add a teachers.js server with a query for the database inside

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'officer',
+  password: '',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+const cohort = process.argv[2];
+pool.query(`
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM assistance_requests
+  JOIN teachers ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name = '${cohort}'
+  ORDER BY teacher;
+`)
+.then(res => {
+  console.log(res.rows);
+})
+.catch(err => console.error('query error', err.stack));
